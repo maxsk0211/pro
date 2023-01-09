e7de29dab7b2760458ffe985a2579871173e91a7
maxsk0211 <121813782+maxsk0211@users.noreply.github.com>
2023-01-09 10:29:31 +0700

    Update del-chat-detail.php

--- users/sql/del-chat-detail.php
+++ users/sql/del-chat-detail.php
@@ -1,8 +1,13 @@
 <?php 
 	session_start();
-	$id_chat_room=$_POST['id_chat_room'];
-	$sql="DELETE FROM `chat_room` WHERE `chat_room`.`id_chat_room` = 5";
-
+	require '../../dbcon.php';
+	$id_chat_room=$_GET['id_chat_room'];
+	$sql="DELETE FROM chat_room WHERE id_chat_room = '$id_chat_room'";
+	$result=mysqli_query($conn,$sql);
+	if ($result) {
+		$_SESSION['ok']="ลบห้องสนทนาสำเร็จ";
+		header("location: ../users-add-chat-room.php");
+	}
 
 
  ?>
