15874546a6215ca3400e61370de17178b19e7bce
maxsk0211 <121813782+maxsk0211@users.noreply.github.com>
2023-01-11 10:54:16 +0700

    Update chk-login.php

--- sql/chk-login.php
+++ sql/chk-login.php
@@ -8,8 +8,6 @@
 	$result=mysqli_query($conn,$sql);
 	
 	$chk_login=mysqli_fetch_object($result);
-	//echo $chk_login->user_level;
-	//exit();
 	if(isset($chk_login->user_level)){
 		
 		// update log
@@ -39,22 +37,5 @@
 		exit();
 	}
 
-
-
-	// if($chk_login->user_level=='1'){
-	// 	$_SESSION['id_user']=$chk_login->id_user;
-	// 	$_SESSION['user_lv']=$chk_login->user_level;
-	// 	header("location: ../admin/index.php");
-	// 	exit();
-	// }elseif($chk_login->user_level=='0'){
-	// 	$_SESSION['id_user']=$chk_login->id_user;
-	// 	$_SESSION['user_lv']=$chk_login->user_level;
-	// 	header("location: ../users/users-add-webboard.php");
-	// 	exit();
-	// }else{
-	// 	$_SESSION['error']="E-mail หรือ รหัสผ่านไม่ถูกต้อง";
-	// 	header("location: ../index.php");
-	// 	exit();
-	// }
 
 ?>
